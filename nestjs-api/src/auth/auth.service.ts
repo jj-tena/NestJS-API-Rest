04995f95f25cc251fd5f3503ed1f6701e3f84528
josejustotena0@gmail.com <josejustotena0@gmail.com>
2023-02-14 10:57:09 +0100

    create project and authentication endpoints

---
+++ nestjs-api/src/auth/auth.service.ts
@@ -0,0 +1,44 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
+import { AuthDto } from './dto';
+import { PrismaService } from '../prisma/prisma.service';
+import * as argon from 'argon2';
+
+@Injectable({})
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    try {
+      const hash = await argon.hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hash,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('Credentials taken');
+      }
+      throw error;
+    }
+  }
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    if (!pwMatches) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+    delete user.hash;
+    return user;
+  }
+}
